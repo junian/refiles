8dc9f8ad8acbf193e6a93a6bf89744e75367466b
Junian Triajianto <author@junian.dev>
2024-05-28 09:01:34 +0700

    Update connectionstring

---
+++ ToolsForHipPOS/Services/LocalSettingService.cs
@@ -0,0 +1,23 @@
+﻿using Plugin.Settings.Abstractions;
+using Plugin.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ToolsForHipPOS.Services
+{
+    internal class LocalSettingService
+    {
+        private ISettings CurrentSettings => CrossSettings.Current;
+
+        public static readonly string DefaultConnectionString = "Data Source=DeskMini-X300;Initial Catalog=Hippos;Persist Security Info=True;User ID=hippos_dbuser;Password=hippos_dbpassword";
+        public string ConnectionString
+        {
+            get => CurrentSettings.GetValueOrDefault(nameof(ConnectionString), DefaultConnectionString);
+            set => CurrentSettings.AddOrUpdateValue(nameof(ConnectionString), value);
+        }
+    }
+}
